0b223cef88216f3daba1f7b8c3f719bb969b968b
Steve <stevelpak@gmail.com>
2022-07-11 14:44:31 +0500

    Create Calc

---
+++ lib/pages/home.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+import 'package:calculator/utils/constants.dart';
+
+class CalcApp extends StatefulWidget {
+  const CalcApp({Key? key}) : super(key: key);
+
+  @override
+  State<CalcApp> createState() => _CalcAppState();
+}
+
+class _CalcAppState extends State<CalcApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: nightBgClr,
+        appBar: AppBar(
+          title: const Text("Calculator"),
+          centerTitle: true,
+          backgroundColor: nightBgClr,
+          elevation: 0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 90),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "6,291/5",
+                  style: TextStyle(
+                    color: lastAction,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "1,258.2",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      color: white,
+                      fontSize: 70,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    button(nightactionsClmnClr, "C"),
+                    button(nightactionsClmnClr, "+/-"),
+                    button(nightactionsClmnClr, "%"),
+                    button(actionsClmnnClr, "÷"),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    button(numbersClr, "7"),
+                    button(numbersClr, "8"),
+                    button(numbersClr, "9"),
+                    button(actionsClmnnClr, "*"),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    button(numbersClr, "4"),
+                    button(numbersClr, "5"),
+                    button(numbersClr, "6"),
+                    button(actionsClmnnClr, "-"),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    button(numbersClr, "1"),
+                    button(numbersClr, "2"),
+                    button(numbersClr, "3"),
+                    button(actionsClmnnClr, "+"),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    button(numbersClr, "."),
+                    button(numbersClr, "0"),
+                    button(numbersClr, "DEL"),
+                    button(actionsClmnnClr, "="),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container button(Color bgclr, String text) {
+    return Container(
+      width: 75,
+      height: 75,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: bgclr,
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Colors
+Color nightBgClr = const Color(0xFF17171C);
+Color lightBgClr = const Color(0xFFF1F2F3);
+Color actionsClmnnClr = const Color(0xFF4B5EFC);
+Color actionsRowColor = const Color(0xFFD2D3DA);
+Color nightactionsClmnClr = const Color(0xFF4E505F);
+Color numbersClr = const Color(0xFF2E2F38);
+Color lastAction = const Color(0xFF747477);
+Color black = Colors.black;
+Color white = Colors.white;
